04a99296c16bdbf138ca576ea24bca3959ba685c
officialvikramsuthar <73328353+officialvikramsuthar@users.noreply.github.com>
2021-05-28 17:15:51 +0530

    scratch card updated

--- src/pages/scratch-card/scratch-card.ts
+++ src/pages/scratch-card/scratch-card.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
+import { ScratchCard, SCRATCH_TYPE } from 'scratchcard-js'
 /**
  * Generated class for the ScratchCardPage page.
  *
@@ -19,6 +19,27 @@ export class ScratchCardPage {
   }
 
   ionViewDidLoad() {
+    this.createNewScratchCard();
+  }
+
+  createNewScratchCard() {
+    const scContainer = document.getElementById('js--sc--container')
+    const sc = new ScratchCard('#js--sc--container', {
+      scratchType: SCRATCH_TYPE.CIRCLE,
+      containerWidth: 300,//scContainer.offsetWidth,
+      containerHeight: 300,
+      imageForwardSrc: 'https://york.com.sa/images/uploaded/media-center/congrats-en.png',
+      //imageBackgroundSrc: './assets/images/scratchcard-background.svg',
+      htmlBackground: '<div class="cardamountcss"><div class="won-amnt">30</div><div class="won-text">Points<br>Won!</div></div>',
+      clearZoneRadius: 40,
+      nPoints: 30,
+      pointSize: 4,
+      callback: () => {
+        console.log('Now the window will reload !')
+      }
+    })
+    // Init
+    sc.init();
   }
 
 }
